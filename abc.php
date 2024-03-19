fb4de32b0249368db8c2fdb6c920743fbfa68c5f
ravishankarsaini <ravishanker21@gmail.com>
2024-03-19 15:53:26 +0530

    add featured2 code

--- abc.php
+++ abc.php
@@ -5,4 +5,5 @@ echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 echo "Hello Ravi";
 echo "Hello RSS";
+echo "This is featured 2 branch";
 ?>
