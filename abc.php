c1b344a92967792a1b87e362f42c86982c7741a8
ravishankarsaini <ravishanker21@gmail.com>
2024-03-19 16:21:26 +0530

    update featured1 and featured2 code

--- abc.php
+++ abc.php
@@ -5,5 +5,5 @@ echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 echo "Hello Ravi";
 echo "Hello RSS";
-echo "This is featured 2 branch";
+echo "This is featured1 and featured 2 are in branch";
 ?>
